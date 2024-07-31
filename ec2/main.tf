5484cfd2ae2781de1cabd0ccbba95872897df939
Johan Pham <johanpham2711@gmail.com>
2024-07-31 16:16:45 +0700

    feature: count, for, output

--- ec2/main.tf
+++ ec2/main.tf
@@ -16,9 +16,14 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "terraform-test-instance" {
   #   ami           = "ami-09b80ee40b23aa576"
   #   ami           = "ami-012c2e8e24e2ae21d"
+  count         = 5
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   tags = {
     Name = "terraform-test-instance"
   }
+}
+
+output "ec2" {
+  value = { for index, instance in aws_instance.terraform-test-instance : format("public_ip#%s", index) => instance.public_ip }
 }
